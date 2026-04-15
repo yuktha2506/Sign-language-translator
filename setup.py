f996b810a5f7fee78d6825ddac22c8c1a8ba647b
Yuktha Shree P <yukthashree456@gmail.com>
2026-04-15 21:14:06 +0530

    Check the libraries

---
+++ setup.py
@@ -0,0 +1,11 @@
+import cv2
+import tensorflow as tf
+import mediapipe as mp
+import numpy as np
+
+print(f"OpenCV Version: {cv2.__version__}")
+print(f"TensorFlow Version: {tf.__version__}")
+print(f"MediaPipe Version: {mp.__version__}")
+print(f"NumPy Version: {np.__version__}")
+
+print("\n--- All libraries imported successfully! ---")
